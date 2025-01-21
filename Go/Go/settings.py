9470f345605208f7649634df77245f80867f1d92
iuryvlasenko <vlasencko.iury1999@yandex.by>
2025-01-21 17:35:55 +0300

    изменения карточек

--- Go/Go/settings.py
+++ Go/Go/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3, 
+    'PAGE_SIZE': 4, 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
